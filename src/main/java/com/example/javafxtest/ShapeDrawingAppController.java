7ebc1db66f51bccd2e5bcba4c732bd870321ffdf
Abdul Mosawer Wadan <160777411+Wadan3@users.noreply.github.com>
2026-05-26 17:42:41 +0430

    Refactor clearCanvas and add resizeCanvas method

--- src/main/java/com/example/javafxtest/ShapeDrawingAppController.java
+++ src/main/java/com/example/javafxtest/ShapeDrawingAppController.java
@@ -175,7 +175,6 @@ public class ShapeDrawingAppController {
         saveState();
         initializeCanvas();
     }
-
     private void resizeCanvas() {
         TextInputDialog dialog = new TextInputDialog(String.valueOf(canvas.getWidth()));
         dialog.setTitle("Resize Canvas");
